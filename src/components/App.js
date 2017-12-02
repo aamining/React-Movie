2e948cc00ca1095f325b156839866771357067e7
Ali <30610753+aamining@users.noreply.github.com>
2017-12-02 18:45:29 +1100

    add buttons and how to add new movies

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Movie from './Movie';
 import {initialMovies} from '../movies';
 import {additionalMovies} from '../movies';
 
+import AddMovie from './AddMovie';
 
 
 class App extends Component {
@@ -19,6 +20,7 @@ class App extends Component {
       movies: initialMovies
     };
     this.loadAdditionalMovies = this.loadAdditionalMovies.bind(this);
+    this.addMovieToGallery = this.addMovieToGallery.bind( this );
   }
 
   render() {
@@ -32,18 +34,28 @@ class App extends Component {
             Object
             .keys(this.state.movies)
             .map(key => <Movie key={key} meta={this.state.movies[key]} />)
+
+
           }
         </div>
         <div className="add-movies"><button onClick={this.loadAdditionalMovies}>Load more...</button></div>
 
-
-      </div>
+        <AddMovie addMovie={this.addMovieToGallery} />      </div>
 
     );
   }
   loadAdditionalMovies() {
     var currentMovies = { ...this.state.movies };
     var newMovies = Object.assign( currentMovies, additionalMovies );
+    this.setState({ movies: newMovies });
+  }
+
+  addMovieToGallery( movie ) {
+    var ts = Date.now();
+    var newMovie = {};
+    newMovie[ 'movie' + ts ] = movie;
+    var currentMovies = { ...this.state.movies };
+    var newMovies = Object.assign( currentMovies, newMovie );
     this.setState({ movies: newMovies });
   }
 }
